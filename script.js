6a13a06f147f83defecc73eef396062e947c2197
Justin Wangsa <justinwangsa9@gmail.com>
2025-07-26 13:32:05 +0800

    for hosting

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
 document.addEventListener('DOMContentLoaded', function () {
+  // Navbar 
   let lastScrollTop = 0;
   const navbar = document.querySelector('.navbar');
 
@@ -7,14 +7,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     if (scrollTop > lastScrollTop) {
-
-      navbar.style.top = "-200px";
+      navbar.style.top = "-200px"; 
     } else {
-
-      navbar.style.top = "2rem";
+      navbar.style.top = "2rem"; 
     }
 
     lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
   });
-});
 
+  // scroll-in animation
+  const observer = new IntersectionObserver((entries, obs) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        obs.unobserve(entry.target);
+      }
+    });
+  }, {
+    threshold: 0.2
+  });
+
+  const scrollAnimatedSections = document.querySelectorAll('.project-wrapper, .skills-wrapper');
+  scrollAnimatedSections.forEach(section => observer.observe(section));
+});
